351c1e6be4f97436c1b134c0eb5587b87357a66f
bupaleo <114641343+bupaleo@users.noreply.github.com>
2022-11-03 02:06:44 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let menu=document.getElementById("nav-icon")
+menu.addEventListener("click",lol)
+function lol(){
+    let x=document.getElementById("minimenu")
+    x.style.display="block"
+    if (display="block") {
+        (display="none")
+    }
+    if (display="none") {
+        (display="block")
+    }
+}
